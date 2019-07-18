880d4bf1b391360047e4f698b35ecc6d0e909e46
filipe cavalcante <41444061+lipscheech@users.noreply.github.com>
2019-07-18 20:05:35 -0300

    Projeto webflux

---
+++ SpringMongoWebflux/src/main/java/com/schoolofnet/SpringMongoWebflux/CustomerResouce.java
@@ -0,0 +1,42 @@
+package com.schoolofnet.SpringMongoWebflux;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/Costumers")
+public class CustomerResouce {
+	
+	@Autowired
+	private CustomerRepository repository;
+	
+	public CustomerResouce(CustomerRepository repository) {
+		this.repository = repository;
+	}
+	
+	@GetMapping
+	public Flux<Customer> finAll(){
+		return this.repository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseBody
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Mono<Customer> create(@RequestBody Customer customer){
+		return this.repository.save(customer);
+	}
+	
+	
+	
+}
